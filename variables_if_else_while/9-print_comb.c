968bee9ce3ecfc29e893b7dce17c25dfcc4a48a2
karen <kiziasuma@gmail.com>
2024-10-25 15:22:52 +0200

    update

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+                putchar('\054');
+                putchar('\44');
+
+	}
+	putchar('\n');
+	return (0);
+}
