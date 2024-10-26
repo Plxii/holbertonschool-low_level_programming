fb86c9a2911b1ce27ca9f7e647a70b247fb473a5
karen <kiziasuma@gmail.com>
2024-10-26 22:21:29 +0200

    update

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 /**
- * _isalpha - Vérifie si un caractère est un alphabet ( a à z et A à Z) ou non.
+ * _isalpha - Vérifie si un caractère est un alphabet ou non.
  * @c: Le caractère à vérifier
  *
  * Return: 1 si c'est une lettre minuscule ou majuscule, sinon 0.
@@ -10,7 +10,7 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z)' || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
