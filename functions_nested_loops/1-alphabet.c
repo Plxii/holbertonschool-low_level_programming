669aa755b68006b7ee7cbc3d98f7e8ac56636f20
karen <kiziasuma@gmail.com>
2024-10-26 16:11:23 +0200

    update

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - imprime l'alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+
+	} _putchar('\n');
+
+	return (0);
+}
