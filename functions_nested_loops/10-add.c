08e881001e9f1ecbf8b0b6326468a1d6b23e26c9
karen <kiziasuma@gmail.com>
2024-10-27 16:33:59 +0100

    update

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Additionner 2 entiers et renvoie la resultat.
+ * @a: Le paramètre
+ * @b: Le paramètre
+ *
+ * Return: Le resultat
+ *
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
