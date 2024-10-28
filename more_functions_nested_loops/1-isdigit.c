3281a5afc29fe96387e7826afbfd0d1bedde01de
karen <kiziasuma@gmail.com>
2024-10-28 17:19:56 +0100

    update

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - Vérifie la presence d'un chiffre (0 à 9)
+ * @c: Le chiffre
+ *
+ * Return: 1 si c est un chiffre sinon 0
+ */
+int _isdigit(int c)
+{
+	if (c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
