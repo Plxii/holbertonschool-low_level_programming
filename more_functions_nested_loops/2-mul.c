d89f1db2af6548caaca3d308f6d84e541d0045dd
karen <kiziasuma@gmail.com>
2024-10-28 23:30:53 +0100

    update

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Multiplie deux nombres entiers.
+ * @a: Le paramètre
+ * @b: Le paramètre
+ *
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	if (a * b && a * b > 0)
+	{
+		return (a * b);
+	}
+	else
+	{
+		return (a * b);
+	}
+}
