9e12bb8326b8dbc51adccf7391edf4d621fc08f1
karen <kiziasuma@gmail.com>
2024-10-28 12:30:47 +0100

    update

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,88 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_to_98 -Imprime rous les nombres
- * ne n a 98.
- * @n: Le paramètre
+ * print_to_98 - Affiche tous les nombres de n à 98.
+ * @n: Le nombre de départ.
  *
  * Return: rien
- *
  */
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0
- */
-
 void print_to_98(int n)
 {
-	int i;
-	int temp;
-
-	if (n < 99)
+	if (n >= 98)
 	{
-		for(i = n ; i < 99 ; i++)
-		{
-			if(i < 0)
-			{
-				_putchar('-');
-				temp = -i;
-				if(temp > 99)
-				{
-					_putchar((temp / 100) + '0');
-					_putchar(((temp / 10) % 10)+ '0');
-					_putchar((temp % 10) + '0');
-				}
-				else if(temp > 9)
-				{
-					_putchar((temp / 10) + '0');
-					_putchar((temp % 10) + '0');
-				}
-				else
-				{
-					_putchar((temp % 10) + '0');
-				}
-			}
-			else if(i > 9)
-			{
-				_putchar((i / 10 ) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else if (i >= 0)
-			{
-				_putchar((i % 10) + '0');
-			}
-			if(i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
+			while (n > 98)
+			printf("%d, ", n--);
+			printf("%d\n", n);
 	}
 	else
 	{
-		for(i=n; i > 97; i--)
-		{
-			if(i > 99)
-			{
-				_putchar((i / 100) + '0');
-				_putchar(((i / 10) % 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else 
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			if(i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
+			while (n < 98)
+			printf("%d, ", n++);
+			printf("%d\n", n);
 	}
 }
