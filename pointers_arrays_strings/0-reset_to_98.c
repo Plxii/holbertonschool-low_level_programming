00fcec1399d6d5bea5efbe04dc358c232004b90e
karen <kiziasuma@gmail.com>
2024-10-31 14:32:35 +0100

    update

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Fonction qui prend un pointeur sur un int
+ * comme paramètre.
+ * @n: Le paramètre
+ *
+ * Return: rien.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
