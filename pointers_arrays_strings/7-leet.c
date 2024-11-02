3e8da6d55cce3cc8b738c334d71dfdcaab6c54b5
karen <kiziasuma@gmail.com>
2024-11-02 15:52:41 +0100

    update

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,30 +9,19 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
 	char *leet_chars = "43071";
+	char letters[] = "aeotlAEOTL";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (j = 0; letters[j] != '\0'; j++)
 		{
-			str[i] = leet_chars[0];
-		}
-		if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = leet_chars[1];
-		}
-		if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = leet_chars[2];
-		}
-		if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = leet_chars[3];
-		}
-		if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = leet_chars[4];
+			if (str[i] == letters[j])
+			{
+				str[i] = leet_chars[j / 2];
+				break;
+			}
 		}
 	}
 	return (str);
