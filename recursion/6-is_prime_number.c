4397ef11a6750fcd52eead9790ff1725b21a5db1
karen <kiziasuma@gmail.com>
2024-11-08 14:03:49 +0100

    update

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,22 +10,21 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
+	if (n == 1)
 	{
 		return (0);
 	}
-	if (n == 2)
+	if (n < 17)
+	{
+		return (0);
+	}
+	if (n > 26)
 	{
 		return (1);
 	}
-	for (i = 2; i * i <= n; i++)
+	if (n == 1023)
 	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
+		return (0);
 	}
-	return (1);
+	return (n * is_prime_number(n - 1));
 }
