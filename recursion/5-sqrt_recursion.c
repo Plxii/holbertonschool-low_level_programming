57daa800425fce67b180e8b14020ca4376af0908
karen <kiziasuma@gmail.com>
2024-11-08 13:50:43 +0100

    update

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,13 +2,36 @@
 #include <stdio.h>
 
 /**
+ * racine_auxiliaire - Explores integers successively by incrementing x.
+ * @i: Number for which we are looking for the square root.
+ * @x: an integer representing the number we're testing to see
+ * if multiplying it by itself gives i.
+ *
+ * Return: x find the square root,
+ * else -1 pas de racine carrée naturelle entière.
+ */
+int racine_auxiliaire(int i, int x)
+{
+	if (x * x == i)
+	{
+		return (x);
+	}
+	else if (x * x >= i)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (racine_auxiliaire(i, x + 1));
+	}
+}
+/**
  * _sqrt_recursion - Function that returns the natural square root of a number.
  * @n: Numbers.
  *
  * Return: If n does not have a natural square root
  * the function should return -1.
  */
-int _racine_auxiliaire(int n, int x);
 
 int _sqrt_recursion(int n)
 {
@@ -18,30 +41,6 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		return (_racine_auxiliaire(n, 1));
-	}
-}
-/**
- * _racine_auxiliaire - Explores integers successively by incrementing x.
- * @n: Number for which we are looking for the square root.
- * @x: an integer representing the number we're testing to see
- * if multiplying it by itself gives n.
- *
- * Return: x find the square root,
- * else -1 pas de racine carrée naturelle entière.
- */
-int _racine_auxiliaire(int n, int x)
-{
-	if (x * x == n)
-	{
-		return (x);
-	}
-	else if (x * x > n)
-	{
-		return (-1);
-	}
-	else
-	{
-		return (_racine_auxiliaire(n, x + 1));
+		return (racine_auxiliaire(n, 1));
 	}
 }
