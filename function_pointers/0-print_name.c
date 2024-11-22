e68f42268793cce682afd0f87f726b3e4e6d0e3e
karen <kiziasuma@gmail.com>
2024-11-22 19:04:05 +0100

    update

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
+#include <stddef.h>
 #include "function_pointers.h"
 /**
  * print_name - Function that prints a name.
  * @name: Name of the person.
  * @f: Function passed.
+ * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (name && f)
 	{
-		return;
+		f(name);
 	}
-	f(name);
 }
