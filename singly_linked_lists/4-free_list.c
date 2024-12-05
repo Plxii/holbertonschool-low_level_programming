0f1356cca4fc00605fef65ca4c32418c4fd4ad76
karen <kiziasuma@gmail.com>
2024-12-05 22:08:21 +0100

    update

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: list.
+ *
+ * Return: nothing.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptr;
+	list_t *i;
+
+	ptr = head;
+
+	while (ptr != NULL)
+	{
+		i = ptr->next;
+		free(ptr->str);
+		free(ptr);
+		ptr = i;
+	}
+}
