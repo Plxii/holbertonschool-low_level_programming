7ee4d86981a8c9bd5d233845b230a20000f5f092
karen <kiziasuma@gmail.com>
2025-01-12 18:35:08 +0100

    update

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,21 +1,25 @@
-#include "list.h"
+#include "lists.h"
 
 /**
- * sum_dlistint - Sums all the data of a dlistint_t list.
- * @head: The head of the dlistint_t list.
- *
- * Return: The sum of all the data.
- */
+* sum_dlistint - function
+* @head: pointer to first node in dbl lnkd list
+*
+* Return: sum of data or 0
+*/
 int sum_dlistint(dlistint_t *head)
 {
+	dlistint_t *temph;
 	int sum = 0;
 
-	while (head)
+	if (head == NULL)
 	{
-		sum += head->n;
-		head = head->next;
+		return (0);
 	}
-
+	temph = head;
+	while (temph != NULL)
+	{
+		sum += temph->n;
+		temph = temph->next;
+	}
 	return (sum);
 }
-
